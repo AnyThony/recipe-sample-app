754bd24afaf80f563f688fe3b1438147458497ab
AnyThony <28424838+AnyThony@users.noreply.github.com>
2020-07-11 20:14:05 -0400

    build

--- build/static/js/4.5d857634.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonprecipe-sample-app"]=this["webpackJsonprecipe-sample-app"]||[]).push([[4],{173:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(17),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.f)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.n)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=4.5d857634.chunk.js.map
