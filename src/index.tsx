b5449cce0193eaf82fa01912eb342418592e963e
AnyThony <28424838+AnyThony@users.noreply.github.com>
2020-07-12 02:28:52 -0400

    trigger resize temp fix

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,11 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// Temp fix to force style SliderList
+setTimeout(() => {
+    window.dispatchEvent(new Event('resize'));
+}, 75);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
